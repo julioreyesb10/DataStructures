d4f214a6b9493c53c32f058c736f5ddf996c28e1
Julz <julio.rgonzalez@alumnos.udg.mx>
2021-10-28 23:55:46 -0500

    adding e2 from chapter 1
    estructura de datos - Cairo

---
+++ e2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    float promedio;
+    printf("Ingrese el numero de estudiantes\n");
+    scanf("%d",&n);
+    
+    return 0;
+}
