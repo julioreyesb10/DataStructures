7524ecdf6105db45cdcad1ff18407eb4b7604808
Julz <julio.rgonzalez@alumnos.udg.mx>
2021-10-29 22:44:53 -0500

    new

--- e2.c
+++ e2.c
@@ -5,8 +5,13 @@ int main()
 {
     int n;
     float promedio;
-    printf("Ingrese el numero de estudiantes\n");
-    scanf("%d",&n);
+    int calificaciones[10];
+
+    printf("ingrese los promedios en orden:\n");
+    for(int i = 0; i<10 ; i++)
+    {
+        scanf("%d",calificaciones[i]);
+    }
     
     return 0;
 }
