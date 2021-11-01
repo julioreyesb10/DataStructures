11f920377450b799e52d9857b3a049adf7fe775b
Julz <julio.rgonzalez@alumnos.udg.mx>
2021-10-31 19:24:54 -0600

    new change

--- e2.c
+++ e2.c
@@ -4,13 +4,26 @@
 int main()
 {
     int n;
-    float promedio;
+    float promedio = 0;
     int calificaciones[10];
-
+    int aprobado = 0;
+    int reprobado = 0;
     printf("ingrese los promedios en orden:\n");
     for(int i = 0; i<10 ; i++)
     {
         scanf("%d",&calificaciones[i]);   
+        promedio = calificaciones[i] + promedio;
+        if(calificaciones[i] >= 60)
+        {
+            aprobado ++;
+        }
+        else{
+            reprobado++;
+        }
     }
+    promedio = promedio/10;
+    printf("Promedio = %f\n", promedio);
+    printf("Aprobados = %d\n", aprobado);
+    printf("Reprobados = %d\n", reprobado);
     return 0;
 }
